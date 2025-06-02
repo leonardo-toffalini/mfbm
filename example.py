464c2d4490d451f1d5f30942411769305479f540
leonardo-toffalini <leonardotoffalini@gmail.com>
2025-06-02 15:56:06 +0200

    first time working

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
 from mfbm.mfbm import MFBM
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=5, suppress=True, linewidth=1000)
 
-def corr_matrix(p, mean_corr):
+def random_corr_matrix(p, mean_corr):
     corr = np.full((p, p), mean_corr)
     noise = np.random.normal(scale=0.1, size=(p, p))
     np.fill_diagonal(noise, 0)
@@ -12,18 +13,36 @@ def corr_matrix(p, mean_corr):
     corr = (corr + corr.T) / 2  # make sure its symmetric
     return np.clip(corr, 0, 1)
 
-H = np.linspace(0.05, 0.25, 2)
-n = 4
+p = 5
+H = np.linspace(0.6, 0.9, 5)
+n = 100
 m = 1 << (2 * n - 1).bit_length()
-p = H.size
-rho = corr_matrix(len(H), 0.8)
+
+random_corr = True
+
+if random_corr:
+    rho = random_corr_matrix(len(H), 0.8)
+else:
+    rho = 0.7 * np.ones((p, p))
+    np.fill_diagonal(rho, 1)
+
 eta = np.ones_like(rho)
 sigma = np.ones(len(H))
 
 mfbm = MFBM(H, n, rho, eta, sigma)
+ts = mfbm.sample()
 
-print(mfbm.C)
-print(mfbm.C.shape)
-print(f"{m}·{p} x {m}·{p}")
-# print(mfbm.circulant_row)
+fig = plt.figure(figsize=(14, 6))
+ax1 = plt.subplot(1, 3, 1)
+ax1.imshow(rho)
+ax1.set_title("Correlations")
+
+ax2 = plt.subplot(1, 3, (2, 4))
+for i in range(len(H)):
+    ax2.plot(range(n), ts[i], label=f'H{i}={H[i]:.2f}', alpha=0.7)
+ax2.legend()
+ax2.set_title("Realizations")
+
+plt.show()
+
 
